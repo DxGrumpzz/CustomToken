2dd86f9823e40a81427a4f78083fb34c1c4cb7a3
Josef Lintz <yosi1.lints@gmail.com>
2019-10-30 15:26:09 +0200

    + Added more "statefull" code in client. This of course is just temporary

--- Source/CustomToken.Client/ClientProgram.cs
+++ Source/CustomToken.Client/ClientProgram.cs
@@ -47,34 +47,81 @@
 
                 _token = loginResponse.Token;
 
+                Console.WriteLine($"Succesfully logged in as {username}. \nReceived token: {_token}");
+                Console.WriteLine($"Press enter to continue");
+                Console.ReadLine();
+
+
                 var authorizedRequest = await _client.PostAsync($"{_connection.HttpUrl}/Account/Authorized",
                     new StringContent(JsonSerializer.Serialize(new AuthorizedRequest()
                     {
                         Token = _token,
                     }), Encoding.UTF8, "application/json"));
 
-                Debugger.Break();
 
-                var signOutRequest = await _client.PostAsync($"{_connection.HttpUrl}/Account/SignOut",
-                   new StringContent(JsonSerializer.Serialize(new SignOutRequest()
-                   {
-                       Token = _token,
-                   }), Encoding.UTF8, "application/json"));
+                if (authorizedRequest.IsSuccessStatusCode == true)
+                {
+                    Console.WriteLine($"Completed authorized request");
+                    Console.WriteLine($"Press enter to continue");
+                    Console.ReadLine();
 
-                Debugger.Break();
+                    var signOutRequest = await _client.PostAsync($"{_connection.HttpUrl}/Account/SignOut",
+                       new StringContent(JsonSerializer.Serialize(new SignOutRequest()
+                       {
+                           Token = _token,
+                       }), Encoding.UTF8, "application/json"));
 
 
-                var authorizedRequest2 = await _client.PostAsync($"{_connection.HttpUrl}/Account/Authorized",
-                    new StringContent(JsonSerializer.Serialize(new AuthorizedRequest()
+                    if (signOutRequest.IsSuccessStatusCode == true)
                     {
-                        Token = _token,
-                    }), Encoding.UTF8, "application/json"));
+                        Console.WriteLine($"Signed out succesfully");
+                        Console.WriteLine($"Press enter to continue");
+                        Console.ReadLine();
 
-                Debugger.Break();
 
+                        var authorizedRequest2 = await _client.PostAsync($"{_connection.HttpUrl}/Account/Authorized",
+                            new StringContent(JsonSerializer.Serialize(new AuthorizedRequest()
+                            {
+                                Token = _token,
+                            }), Encoding.UTF8, "application/json"));
+
+                        if (authorizedRequest2.IsSuccessStatusCode == false)
+                        {
+                            Console.WriteLine($"Authorized request failed succesfully");
+                            Console.WriteLine($"Press enter to exit");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Authorization request was a success when it should have failed");
+                            Console.ResetColor();
+                            Console.ReadLine();
+                        };
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Sign out request failed");
+                        Console.ResetColor();
+                        Console.ReadLine();
+                    };
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Authorization request failed");
+                    Console.ResetColor();
+                    Console.ReadLine();
+                };
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Login failed");
+                Console.ResetColor();
+                Console.ReadLine();
             };
-
-            Debugger.Break();
         }
 
     };
