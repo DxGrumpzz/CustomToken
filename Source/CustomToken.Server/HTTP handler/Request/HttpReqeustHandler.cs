5fef4e97b97491cc3c2adb3120abf898d6fcc0bf
Josef Lintz <yosi1.lints@gmail.com>
2019-10-31 13:53:06 +0200

    + Added comments to HttpRequestHandler

--- Source/CustomToken.Server/HTTP handler/Request/HttpReqeustHandler.cs
+++ Source/CustomToken.Server/HTTP handler/Request/HttpReqeustHandler.cs
@@ -1,11 +1,15 @@
 ﻿namespace CustomToken.Server
 {
     /// <summary>
-    /// 
+    /// A class that is responsible for handling incoming HTTP requests
     /// </summary>
     public class HttpReqeustHandler
     {
-
+        /// <summary>
+        /// Creates a  new <see cref="HttpRequest"/> and fills it with data
+        /// </summary>
+        /// <param name="rawRequestString"> The raw header/request </param>
+        /// <returns></returns>
         public HttpRequest CreateRequest(string rawRequestString)
         {
             return new HttpRequest(rawRequestString);
